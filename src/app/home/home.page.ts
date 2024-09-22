ea110e3f4a7e1e44b0fc08a04deda4422ca86d36
raissanrbna <raissanirbana19@gmail.com>
2024-09-22 12:03:34 +0700

    PGPBL_Acara5

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,8 +4,11 @@ import MapView from '@arcgis/core/views/MapView';
 import { Geolocation } from '@capacitor/geolocation';
 import Graphic from '@arcgis/core/Graphic';
 import Point from '@arcgis/core/geometry/Point';
-
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 import PictureMarkerSymbol from '@arcgis/core/symbols/PictureMarkerSymbol';
+import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
+import GroupLayer from '@arcgis/core/layers/GroupLayer';
+import Basemap from '@arcgis/core/Basemap'
 
 @Component({
   selector: 'app-home',
@@ -13,71 +16,136 @@ import PictureMarkerSymbol from '@arcgis/core/symbols/PictureMarkerSymbol';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  mapView: MapView | any;
+  userLocationGraphic: Graphic | any;
+  selectedBasemap!: string;
 
   constructor() { }
-  latitude!: number;
-  longitude!: number;
 
-  public async ngOnInit() {
-    
-    // Mengambil posisi GPS
-    const position = await Geolocation.getCurrentPosition();
-    this.latitude = position.coords.latitude;
-    this.longitude = position.coords.longitude;
-
-    // //Lokasi tetap sesuai koordinat
-    // this.longitude = 110.36711541835224,
-    //   this.latitude = -7.78219668228537
-
+  async ngOnInit() {
     const map = new Map({
-      // basemap: "topo-vector"
-      // basemap:"gray"
       basemap: "streets"
     });
-    const view = new MapView({
+
+    this.mapView = new MapView({
       container: "container",
       map: map,
-      zoom: 10,
-      center: [this.longitude, this.latitude]
+      zoom: 8
     });
 
-    //Membuat objek marker lokasi
-    const point = new Point({
-      longitude: 110.3778691609815,
-      latitude: -7.770034913764278,
+    let weatherServiceFL = new ImageryLayer({ url: WeatherServiceUrl });
+    map.add(weatherServiceFL);
+
+    this.addWeatherServiceMarker();
+
+    await this.updateUserLocationOnMap();
+    this.mapView.center = this.userLocationGraphic.geometry as Point; //Map center sesuai geolocation
+    setInterval(this.updateUserLocationOnMap.bind(this), 100000);
+  }
+
+  //Mengubah Basemap
+  async changeBasemap() {
+    this.mapView.map.basemap = this.selectedBasemap;
+  }
+
+  //Current Location Pengguna
+  async getLocationService(): Promise<number[]> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((resp) => {
+        resolve([resp.coords.latitude, resp.coords.longitude]);
+      });
     });
+  }
 
-    // // Array berisi beberapa koordinat titik
-    // const locations = [
-    //   { longitude: 110.3778691609815, latitude: -7.770034913764278 },
-    //   { longitude: 110.36711541835224, latitude: -7.78219668228537 },
-    //   { longitude: 110.355072, latitude: -7.789034 }, // Tambah titik lainnya di sini
-    // ];
+  //Update Current Location Pengguna
+  async updateUserLocationOnMap() {
+    let latLng = await this.getLocationService();
+    let geom = new Point({ latitude: latLng[0], longitude: latLng[1] });
+    if (this.userLocationGraphic) {
+      this.userLocationGraphic.geometry = geom;
+    } else {
+      this.userLocationGraphic = new Graphic({
+        symbol: new SimpleMarkerSymbol(),
+        geometry: geom,
+      });
+      this.mapView.graphics.add(this.userLocationGraphic);
+    }
+  }
 
-    // // Iterasi untuk membuat dan menambahkan tiap marker
-    // locations.forEach(location => {
-    // // Membuat objek Point untuk setiap lokasi
-    //   const point = new Point({
-    //     longitude: location.longitude,
-    //     latitude: location.latitude
-    //   });
+  //Marker di Lokasi Weather Service
+  addWeatherServiceMarker() {
+    const locations = [
+      { latitude: 38.859216863568335, longitude: -92.9682588257259 },
+      { latitude: 40.26731343533949, longitude: -94.11128898454433 },
+      { latitude: 44.42829525541613, longitude: -88.95474304557064 },
+      { latitude: 41.569457859013134, longitude: -93.5846014925945 }
+    ];
 
-      // Membuat marker dengan gambar
+    // Iterasi melalui setiap lokasi
+    locations.forEach(location => {
+      const weatherServiceLocation = new Point({
+        latitude: location.latitude,
+        longitude: location.longitude,
+      });
+
+      //Mengatur tampilan simbol
       const markerSymbol = new PictureMarkerSymbol({
         type: "picture-marker",
-        url: "assets/location2.png", // Gunakan gambar marker sesuai kebutuhan
+        url: "assets/location2.png", // Gambar marker
         width: "35px",
         height: "35px"
       });
-
-      // Membuat objek Graphic untuk tiap marker
       const pointGraphic = new Graphic({
-        geometry: point,
+        geometry: weatherServiceLocation,
         symbol: markerSymbol
       });
 
-      // Menambahkan graphic ke peta
-      view.graphics.add(pointGraphic);
+      // Tambahkan marker ke peta
+      this.mapView.graphics.add(pointGraphic);
+    });
   }
-}
 
+}
+const WeatherServiceUrl =
+  'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer'
+
+
+// //Membuat objek marker lokasi
+// const point = new Point({
+//   // longitude: 110.3778691609815,
+//   // latitude: -7.770034913764278,
+//   longitude: this.longitude,
+//   latitude: this.latitude,
+// });
+
+// // Array berisi beberapa koordinat titik
+// const locations = [
+//   { longitude: 40.3395834702893, latitude: -94.11128898454433},
+//   { longitude: 49.910567191006045, latitude: -95.3772383660649 },
+//   { longitude: 49.81014697521505, latitude: -94.46364800451474 }, // Tambah titik lainnya di sini
+// ];
+
+// // Iterasi untuk membuat dan menambahkan tiap marker
+// locations.forEach(location => {
+// // Membuat objek Point untuk setiap lokasi
+//   const point = new Point({
+//     longitude: location.longitude,
+//     latitude: location.latitude
+//   });
+
+// // Membuat marker dengan gambar
+// const markerSymbol = new PictureMarkerSymbol({
+//   type: "picture-marker",
+//   url: "assets/location2.png", // Gunakan gambar marker sesuai kebutuhan
+//   width: "35px",
+//   height: "35px"
+// });
+
+// // Membuat objek Graphic untuk tiap marker
+// const pointGraphic = new Graphic({
+//   geometry: point,
+//   symbol: markerSymbol
+// });
+
+// // Menambahkan graphic ke peta
+// view.graphics.add(pointGraphic);
